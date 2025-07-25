f2d3ee38abe2f8be623a880d8f56d018ba556e3f
AnnaEls <86211452+AnnaEls@users.noreply.github.com>
2025-07-25 21:43:15 +0200

    Update train.py

--- Training/train.py
+++ Training/train.py
@@ -9,7 +9,7 @@ from STEMImageDenoising.Training.mask import random_patch_mask
 from STEMImageDenoising.Utilities.utils import save_model
 import numpy as np
 
-def train_model(model, input, path, learning_rate=1e-3,  num_iter=1, patch_size=1, mask_ratio=0.2):
+def train_model(model, input, path, learning_rate=1e-3,  num_iter=1, patch_size=1, mask_ratio=0.2, show_image=False):
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
   model = model.to(device)
   optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
@@ -31,7 +31,7 @@ def train_model(model, input, path, learning_rate=1e-3,  num_iter=1, patch_size=
      loss.backward()
      optimizer.step()
 
-     save_model(it, loss, model, optimizer, path, input)
+     save_model(it, loss, model, optimizer, path, input, show_image)
     
 
   np.save(path+'loss_history.npy', np.array(loss_history))
